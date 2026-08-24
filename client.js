087e0e55e16a3402525f9c95174011bf0a720a20
mallavarapu srividya <srividyamallavarapu2@gmail.com>
2026-08-25 00:58:51 +0530

    Change client import to use ESM URL

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const supabaseUrl = 'https://qepfefzbdqtpicaeyjcu.supabase.co'
 const supabasePublishableKey = 'sb_publishable_5IxAFqMSm17tjN8xBYcd1w_elk8WDoF'
